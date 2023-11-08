ed4cb9a2776e302d2dc91ca88243187eb1a395e8
Carter Hanson <cahanson18@gmail.com>
2023-11-08 01:04:51 +0000

    connecting to espn api

--- espnsportsapi.go
+++ espnsportsapi.go
@@ -1,13 +1,14 @@
 package espnsportsapi
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
 func GetScoreboard(sport string, league string, date string) string {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	resp, err := http.Get(fmt.Sprintf("http://site.api.espn.com/apis/site/v2/sports/%s/%s/scoreboard?date=%s", sport, league, date))
 	if err != nil {
 		log.Fatalln(err)
 	}
