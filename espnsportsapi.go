e1c5c788868bc230fc7b8c65df4bfe56719a758e
Carter Hanson <cahanson18@gmail.com>
2023-11-08 00:54:37 +0000

    go work :sob:

--- espnsportsapi.go
+++ espnsportsapi.go
@@ -1,20 +1,10 @@
-package espnsportsapi
+package greetings
 
-import (
-	"io"
-	"log"
-	"net/http"
-)
+import "fmt"
 
-func GetScoreboard(sport string, league string, date string) string {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	sb := string(body)
-	return sb
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
